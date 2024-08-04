a876ee73f8f3ef997f7087448b18c910af7abef1
Walter Alvarado <walvdev@gmail.com>
2024-08-04 12:18:09 -0500

    remove imports

--- src/components/common/CostDriver.tsx
+++ src/components/common/CostDriver.tsx
@@ -1,10 +1,9 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import {
   Box,
   FormLabel,
   Radio,
   RadioGroup,
-  Stack,
   Text,
   Tooltip,
   Table,
